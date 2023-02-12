f6841aa4fe3bfbdd9b102d5d637a3a38999b696f
minyeokang <dobieborie1@gmail.com>
2023-02-12 21:28:33 +0900

    add router

---
+++ domain/question/question_router.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from database import get_db
+from models import Question
+
+router = APIRouter(
+    prefix="/api/question",
+)
+
+
+@router.get("/list")
+def question_list(db: Session = Depends(get_db)):
+    with get_db() as db:
+        _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+    return _question_list
