b39fba3b484f98877a560626341a4059a77cc7d2
minyeokang <dobieborie1@gmail.com>
2023-02-20 21:38:59 +0900

    add router, env file, js file to fetch by certain urls

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,19 +1,10 @@
-<!-- connect frontend backend  -->
 <script>
-  let question_list = []
+  import Router from 'svelte-spa-router'
+  import Home from "./routes/Home.svelte"
 
-function get_question_list() {
-  fetch("http://127.0.0.1:8000/api/question/list").then((response) => {
-    response.json().then((json) => {
-      question_list = json
-    })
-  })
-}
-
-get_question_list()
+  const routes = {
+    '/': Home,
+  }
 </script>
-<ul>
-  {#each question_list as question}
-    <li>{question.subject}</li>
-  {/each}
-</ul>
+
+<Router {routes}/>
---
+++ frontend/src/routes/Home.svelte
@@ -0,0 +1,18 @@
+<!-- connect frontend backend  -->
+<script>
+    import fastapi from "../lib/api";
+    let question_list = []
+  //빈 배열 만들고 데이터 조회하여 얻은 데이터 변수에 넣고 호출하기
+  function get_question_list() {
+    fastapi('get', '/api/question/list', {}, (json) => {
+            question_list = json
+        })
+  }
+  
+  get_question_list()
+  </script>
+  <ul>
+    {#each question_list as question}
+      <li>{question.subject}</li>
+    {/each}
+  </ul>
