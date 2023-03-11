87a07eb4f4f34cde715575660d66e07cf72d7874
minyeokang <dobieborie1@gmail.com>
2023-03-11 19:11:36 +0900

    add textarea, submit btn, connect to real answers, draw them on svelte

--- domain/answer/answer_schema.py
+++ domain/answer/answer_schema.py
@@ -1,3 +1,5 @@
+import datetime
+
 from pydantic import BaseModel, validator
 
 
@@ -9,3 +11,11 @@ class AnswerCreate(BaseModel):
         if not v or not v.strip():
             raise ValueError('빈 값은 허용되지 않습니다.')
         return v
+
+class Answer(BaseModel):
+    id: int
+    content: str
+    create_date: datetime.datetime
+
+    class Config:
+        orm_mode = True
