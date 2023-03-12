9be3ee83294d2589cf3b258638d20984cc894f60
minyeokang <dobieborie1@gmail.com>
2023-03-12 18:42:07 +0900

    decorate with bootstrap

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -12,10 +12,27 @@
   
   get_question_list()
   </script>
-  <ul>
-    {#each question_list as question}
-      <li><a use:link href="/detail/{question.id}">{question.subject}</a></li>
-    {/each}
-  </ul>
+  <div class="container my-3">
+    <table class="table">
+        <thead>
+        <tr class="table-dark">
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성일시</th>
+        </tr>
+        </thead>
+        <tbody>
+        {#each question_list as question, i}
+        <tr>
+            <td>{i+1}</td>
+            <td>
+                <a use:link href="/detail/{question.id}">{question.subject}</a>
+            </td>
+            <td>{question.create_date}</td>
+        </tr>
+        {/each}
+        </tbody>
+    </table>
+</div>
   <!-- use:link로 ur에 해시 # 를 사용하는 이유는 
 해시 뒤에 오는 게 어떤 페이지이던 브라우저가 동일한 페이지로 인식하게 하여 프론트단에만 사용하는 페이지를 만들어 서버 에러가 나지 않도록 함  -->
